0c77a33254f46ce998880f71d427bdedbb75d32e
RuslanVelikazoff <123361452+RuslanVelikazoff@users.noreply.github.com>
2024-04-05 13:10:16 +0300

    Add interact objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,9 +1,69 @@
+using System;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] 
+    private bool testing;
+    
+    [SerializeField]
+    private Transform counterTopPoint;
+    
+    [SerializeField]
+    private KitchenObjectSO kitchenObjectSo;
+
+    [SerializeField] 
+    private ClearCounter secondClearCounter;
+
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
+
     public void Interact()
     {
-        Debug.Log("Interact");
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
